f4596c0032f6ec5019bfcc6ded2a051b97602ebf
jose <joseluis.contreras.santos@gmail.com>
2017-10-26 12:11:19 +0200

    Renaming files to meet conventions

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
+import io
+import sys
 import decoder
 import encoder
 import pywt 
 
 def main():
-	#  =      "++00100+10+-+-001-"
-	st = list("CCAATAACTACGCGAATG")
-	sym = {"0":"A", "1":"T", "+":"C", "-":"G"} 
-	
-	d = StackRunDecoder(sym)
-	d.decode(st)
-
 
 if __name__ == "__main__":
-	main()
+    main()
