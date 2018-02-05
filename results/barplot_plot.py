bb4b215b10ae38a71da055b5f588fa2680e477ed
lobiaminor <riccardo.lobianco@mail.polimi.it>
2018-02-05 17:42:06 +0100

    Added final settings.ini

--- results/barplot_plot.py
+++ results/barplot_plot.py
@@ -1,7 +1,9 @@
 from matplotlib import pylab
 import plotly.plotly as py
 
-log = plt.figure()
+log = pylab.figure()
+
+
 
 data = ((3.74,2.02,2.03), (3.58,2.09,1.95), (3.64,2.20,1.79), (3.53,2.30,1.89), (3.72,2.13,1.89), (3.36,2.20,1.91), (2.88,2.34,1.83), (3.79,1.97,1.93), (3.72,2.11,1.96), (3.65,2.10,1.97), (3.65,2.17,2.06), (3.86,2.03,2.10))
 
@@ -14,9 +16,9 @@ dim = len(data[0])
 w = 0.75
 dimw = w / dim
 
-x = ('lake', 'woman_blonde', 'woman_darkhair', 'cameraman', 'lena', 'jetplane', 'house', 'peppers_gray', 'livingroom', 'pirate', 'mandril_gray', 'walkbridge')
+x = ('lake', 'blonde', 'darkhair', 'cameraman', 'lena', 'jet', 'house', 'peppers', 'livingroom', 'pirate', 'mandril', 'bridge')
 for i in range(len(data[0])) :
     y = [d[i] for d in data]
-    b = pylab.bar(x + i * dimw, y, dimw, bottom=0.001)
+    b = pylab.bar(x, y, dimw, bottom=0.001)
 
 plot_url = py.plot_mpl(log, filename='mpl-log')
